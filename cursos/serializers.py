221fc8f18e950a34542989f494cd5a99d3be2cbc
ribeirin-7 <arthurr366@gmail.com>
2025-03-27 13:48:48 -0300

    correção no serializers.py

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,32 +1,17 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
 
-class AvaliacaoSerializer(serializers.Serializer):
+class AvaliacaoSerializer(serializers.ModelSerializer):
     
     class Meta:
         extra_kwargs = {
             'email': {'write_only': True}
         }
         model = Avaliacao
-        fields = (
-            'id',
-            'curso',
-            'nome',
-            'email',
-            'comentario',
-            'avaliacao',
-            'criacao',
-            'ativo'
-        )
+        fields = '__all__'
 
-class CursoSerializer(serializers.Serializer):
+class CursoSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Curso
-        fields = (
-            'id',
-            'titulo',
-            'url',
-            'criacao',
-            'ativo',
-        )
+        fields = '__all__'
