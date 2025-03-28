1380f6ce639e0122d107e800fac910c8fcba329b
ribeirin-7 <arthurr366@gmail.com>
2025-03-28 11:31:12 -0300

    Mudança nas views (utilizando viewsets)

--- cursos/views.py
+++ cursos/views.py
@@ -1,31 +1,13 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
+class CursosViewSet(viewsets.ModelViewSet):
 
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-class AvaliacaoAPIView(APIView):
+class AvaliacaoAPIView(viewsets.ModelViewSet):
     
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
